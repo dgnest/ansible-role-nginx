e78af29506c1805c79e05aa57a9e0837698cf3c4
@slovacus <slovacus@gmail.com>
2016-12-01 18:23:15 -0500

    [#13] Fix: clean code unnecesary

--- script/sync.sh
+++ script/sync.sh
@@ -3,10 +3,3 @@
 
 # shellcheck source=script/bootstrap.sh
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
-
-echo $(date)
-
-echo 'Progress'
-
-rsync --chmod=ug=rwX -e "ssh -i $KEY_FILE" -axv \
-      $DEPLOY_SOURCE_DIR $DEPLOY_ACCOUNT@$DEPLOY_SERVER:$DEPLOY_DEST_DIR --progress
